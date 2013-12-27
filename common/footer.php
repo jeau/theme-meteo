f7b5c0021f27ba0cb76111ee88bcdfea43f009ce
jeau <thierrypasquier@gmail.com>
2013-12-28 00:14:55 +0100

    premiers tests

--- common/footer.php
+++ common/footer.php
@@ -1,17 +1,28 @@
 </div><!-- end content -->
 
 <footer>
+	<?php if ( $footerText = get_theme_option('Footer Text') ): ?>
+        	<div id="custom-footer-text">
+           		<p><?php echo $footerText; ?></p>
+        	</div>
+        <?php endif; ?>
+	<?php if ( $footerCol1 = get_theme_option('Footer Col1') ): ?>
+        	<div class="col1">
+           		<?php echo $footerCol1; ?>
+        	</div>
+        <?php endif; ?>
+	<?php if ( $footerCol2 = get_theme_option('Footer Col2') ): ?>
+        	<div class="col2">
+           		<?php echo $footerCol2; ?>
+        	</div>
+        <?php endif; ?>
+	<?php if ( $footerCol3 = get_theme_option('Footer Col3') ): ?>
+        	<div class="col3">
+           		<?php echo $footerCol3; ?>
+        	</div>
+        <?php endif; ?>
 
-        <div id="custom-footer-text">
-            <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
-            <p><?php echo $footerText; ?></p>
-            <?php endif; ?>
-            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-                <p><?php echo $copyright; ?></p>
-            <?php endif; ?>
-        </div>
-
-        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+	<p><?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')) echo $copyright . ". " ; echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
         
     <?php fire_plugin_hook('public_footer'); ?>
     
